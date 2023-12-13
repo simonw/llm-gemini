faab0a14470712bbb84320ceeb1cf6613eebe413
Simon Willison <swillison@gmail.com>
2023-12-13 12:20:41 -0800

    Initial prototype, refs #1

---
+++ README.md
@@ -0,0 +1,58 @@
+# llm-gemini
+
+[![PyPI](https://img.shields.io/pypi/v/llm-gemini.svg)](https://pypi.org/project/llm-gemini/)
+[![Changelog](https://img.shields.io/github/v/release/simonw/llm-gemini?include_prereleases&label=changelog)](https://github.com/simonw/llm-gemini/releases)
+[![Tests](https://github.com/simonw/llm-gemini/workflows/Test/badge.svg)](https://github.com/simonw/llm-gemini/actions?query=workflow%3ATest)
+[![License](https://img.shields.io/badge/license-Apache%202.0-blue.svg)](https://github.com/simonw/llm-gemini/blob/main/LICENSE)
+
+API access to Google's Gemini models
+
+## Installation
+
+Install this plugin in the same environment as [LLM](https://llm.datasette.io/).
+```bash
+llm install llm-gemini
+```
+## Usage
+
+Configure the model by setting a key called "gemini" to your API key:
+
+```bash
+llm keys set gemini
+```
+```
+<paste key here>
+```
+
+Now run the model using `-m gemini-pro`, for example:
+
+```bash
+llm -m gemini-pro "A joke about a pelican and a walrus"
+```
+
+> Why did the pelican get mad at the walrus?
+>
+> Because he called him a hippo-crit.
+
+To chat interactively with the model, run `llm chat`:
+
+```bash
+llm chat -m gemini-pro
+```
+
+## Development
+
+To set up this plugin locally, first checkout the code. Then create a new virtual environment:
+```bash
+cd llm-gemini
+python3 -m venv venv
+source venv/bin/activate
+```
+Now install the dependencies and test dependencies:
+```bash
+llm install -e '.[test]'
+```
+To run the tests:
+```bash
+pytest
+```
---
+++ llm_gemini.py
@@ -0,0 +1,76 @@
+import httpx
+import ijson
+import llm
+import urllib.parse
+
+# We disable all of these to avoid random unexpected errors
+SAFETY_SETTINGS = [
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE",
+    },
+]
+
+
+@llm.hookimpl
+def register_models(register):
+    register(GeminiPro())
+
+
+class GeminiPro(llm.Model):
+    model_id = "gemini-pro"
+    can_stream = True
+
+    def build_messages(self, prompt, conversation):
+        if not conversation:
+            return [{"role": "user", "parts": [{"text": prompt.prompt}]}]
+        messages = []
+        for response in conversation.responses:
+            messages.append({"role": "user", "parts": [{"text": response.prompt.prompt}]})
+            messages.append({"role": "model", "parts": [{"text": response.text()}]})
+        messages.append({"role": "user", "parts": [{"text": prompt.prompt}]})
+        return messages
+
+    def execute(self, prompt, stream, response, conversation):
+        key = llm.get_key("", "gemini", "LLM_GEMINI_KEY")
+        url = (
+            "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:streamGenerateContent?"
+            + urllib.parse.urlencode({"key": key})
+        )
+        gathered = []
+        with httpx.stream(
+            "POST",
+            url,
+            timeout=None,
+            json={
+                "contents": self.build_messages(prompt, conversation),
+                "safetySettings": SAFETY_SETTINGS,
+            },
+        ) as http_response:
+            events = ijson.sendable_list()
+            coro = ijson.items_coro(events, "item")
+            for chunk in http_response.iter_bytes():
+                coro.send(chunk)
+                if events:
+                    event = events[0]
+                    if isinstance(event, dict) and "error" in event:
+                        raise llm.ModelError(event["error"]["message"])
+                    try:
+                        yield event["candidates"][0]["content"]["parts"][0]["text"]
+                    except KeyError:
+                        yield ""
+                    gathered.append(event)
+                    events.clear()
+        response.response_json = gathered
