c092a437b917b4221fde8a6dbb7e13686d1e8ddb
Simon Willison <swillison@gmail.com>
2024-11-17 23:25:05 -0800

    Test for async

--- tests/test_gemini.py
+++ tests/test_gemini.py
@@ -6,7 +6,8 @@ GEMINI_API_KEY = os.environ.get("PYTEST_GEMINI_API_KEY", None) or "gm-..."
 
 
 @pytest.mark.vcr
-def test_prompt():
+@pytest.mark.asyncio
+async def test_prompt():
     model = llm.get_model("gemini-1.5-flash-latest")
     model.key = model.key or GEMINI_API_KEY
     response = model.prompt("Name for a pet pelican, just the name")
@@ -20,3 +21,9 @@ def test_prompt():
             "modelVersion": "gemini-1.5-flash-002",
         }
     ]
+    # And try it async too
+    async_model = llm.get_async_model("gemini-1.5-flash-latest")
+    async_model.key = async_model.key or GEMINI_API_KEY
+    response = await async_model.prompt("Name for a pet pelican, just the name")
+    text = await response.text()
+    assert text == "Percy"
