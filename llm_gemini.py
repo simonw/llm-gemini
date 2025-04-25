eca96b837eb6dab8ab35ab225a67c0d2a7fde8ee
James Stout <james.wolf.stout@gmail.com>
2025-04-24 21:35:01 -0700

    Added 2.5 Flash to Google search models list.

--- llm_gemini.py
+++ llm_gemini.py
@@ -38,6 +38,7 @@ GOOGLE_SEARCH_MODELS = {
     "gemini-2.0-flash",
     "gemini-2.5-pro-preview-03-25",
     "gemini-2.5-pro-exp-03-25",
+    "gemini-2.5-flash-preview-04-17",
 }
 
 # Older Google models used google_search_retrieval instead of google_search
