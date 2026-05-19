a0f538fba067b7eb268d521e7d084c14330c5323
Simon Willison <swillison@gmail.com>
2026-05-19 12:20:29 -0700

    gemini-3.5-flash, refs #133

--- llm_gemini.py
+++ llm_gemini.py
@@ -57,6 +57,7 @@ GOOGLE_SEARCH_MODELS = {
     "gemini-3.1-pro-preview-customtools",
     "gemini-3.1-flash-lite-preview",
     "gemini-3.1-flash-lite",
+    "gemini-3.5-flash",
 }
 
 # Older Google models used google_search_retrieval instead of google_search
@@ -96,6 +97,7 @@ MODEL_THINKING_LEVELS = {
     "gemini-3.1-pro-preview-customtools": ["low", "medium", "high"],
     "gemini-3.1-flash-lite-preview": ["minimal", "low", "medium", "high"],
     "gemini-3.1-flash-lite": ["minimal", "low", "medium", "high"],
+    "gemini-3.5-flash": ["minimal", "low", "medium", "high"],
 }
 
 NO_VISION_MODELS = {"gemma-3-1b-it", "gemma-3n-e4b-it"}
@@ -222,6 +224,8 @@ def register_models(register):
         "gemma-4-31b-it",
         # 7th May 2026
         "gemini-3.1-flash-lite",
+        # 19th Mary 2026
+        "gemini-3.5-flash",
     ):
         can_google_search = model_id in GOOGLE_SEARCH_MODELS
         can_thinking_budget = model_id in THINKING_BUDGET_MODELS
